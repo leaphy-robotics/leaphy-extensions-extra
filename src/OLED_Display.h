58a6dd602d62725e947aebeb78159aacd973daa4
koen1711 <98043234+koen1711@users.noreply.github.com>
2022-12-02 15:37:38 +0100

    Update OLED_Display.h

--- src/OLED_Display.h
+++ src/OLED_Display.h
@@ -37,6 +37,7 @@ class OLEDDISPLAY
     void println(unsigned long, int = DEC);
     void println(double, int = 2);
     void println(void);
+	void drawBitmap(int16_t x, int16_t y, const uint8_t * bitmap, int16_t w, int16_t h, uint16_t color);
 };
 
 #endif
