974f78845d20d1705a70c6398eb4cc5ba553e6c7
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2023-10-29 13:53:18 +0100

    chore: add compile tests for PRs (#17)

--- src/ledstrip.h
+++ src/ledstrip.h
@@ -1,7 +1,6 @@
 #ifndef __ledstrip__
 #define __ledstrip__
 
-#include <Arduino.h>
 #include "Adafruit_NeoPixel.h"
 
 extern int _snelHeid;
@@ -10,8 +9,8 @@ extern int _delayTime;
 class LEDSTRIP
 {
 	public:
-    LEDSTRIP(uint8_t, uint16_t);
-    void runFunction(uint8_t, uint8_t, uint8_t, uint8_t);
+                LEDSTRIP(uint8_t, uint16_t);
+                void runFunction(uint8_t, uint8_t, uint8_t, uint8_t);
 		void singleColor(uint8_t, uint8_t, uint8_t);
 		void breathe(uint8_t, uint8_t, uint8_t);
 		void marquee(int delayTime, uint8_t, uint8_t, uint8_t);
@@ -19,13 +18,12 @@ class LEDSTRIP
 		void rainbowmarquee(uint8_t, uint8_t, uint8_t);
 				void basis(uint8_t,uint8_t, uint8_t, uint8_t);
 	private:
-    uint32_t Wheel(byte);
-    uint8_t pin;
-    uint16_t total_leds;
-		uint32_t color;
-    // set strip to 60 leds , at pin 8
-    Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 8, NEO_GRB + NEO_KHZ800);
-
+                uint32_t Wheel(byte);
+                uint8_t pin;
+                uint16_t total_leds;
+                uint32_t color;
+                // set strip to 60 leds , at pin 8
+                Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 8, NEO_GRB + NEO_KHZ800);
 };
 
 #endif
---
+++ tests/tests.ino
@@ -0,0 +1,53 @@
+#include "OLED_Display.h"
+#include "ledstrip.h"
+
+LEDSTRIP ledstrip(1, 60);
+
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_16X);
+uint16_t RawColor_Red, RawColor_Green, RawColor_Blue, RawColor_Clear;
+byte Color_Red, Color_Green, Color_Blue, Color_Clear;
+
+double getColor(int colorCode, bool isRaw) {
+  tcs.getRawData(&RawColor_Red, &RawColor_Green, &RawColor_Blue, &RawColor_Clear);
+  Color_Red = min(RawColor_Red/5,255); Color_Green = min(RawColor_Green/5,255); Color_Blue = min(RawColor_Blue/5,255);
+  switch(colorCode) {
+    case 0:
+      return (isRaw) ? RawColor_Red : Color_Red;
+    case 1:
+      return (isRaw) ? RawColor_Green : Color_Green;
+    case 2:
+      return (isRaw) ? RawColor_Blue : Color_Blue;
+  }
+}
+
+OLEDDISPLAY display;
+void leaphyProgram() {
+  _snelHeid = 1;
+  ledstrip.runFunction(4, getColor(0, true), getColor(1, true), getColor(2, true));
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println("Hallo");
+  display.display();
+}
+
+void setup() {
+  if (tcs.begin()) {
+    Serial.println("RGB-sensor gevonden!");
+  } else {
+    Serial.println("Geen RGB-sensor gevonden... check je verbindingen...");
+  }
+  Serial.begin(115200);
+  if(!display.begin())
+  {
+    Serial.println(F("Contact with the display failed: Check the connections"));
+  }
+
+
+  leaphyProgram();
+}
+
+void loop() {
+
+}
